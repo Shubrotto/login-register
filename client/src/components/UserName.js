960d4b4fd1bab274fde6d6cbe84ed8438b854568
Shubrotto Kumar Hawlader <skshubroto@gmail.com>
2023-09-12 12:28:42 +0600

    Initilize project

---
+++ client/src/components/UserName.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const UserName = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div>
+          <div className="title">
+            <h4>Hello Again</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserName;
